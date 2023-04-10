b1c287de35d1f10c7f0f1e780761e2ef65139b0a
le trung han <han@gmail.com>
2023-04-10 21:57:40 +0700

    update casio

---
+++ casio/index.js
@@ -0,0 +1,10 @@
+var numbers= document.querySelectorAll('.numbers');
+var item2 = document.querySelectorAll('.item2');
+var result = document.querySelector('.result');
+var clears = document.querySelector('.clears');
+var negative = document.querySelector('.negative');
+var persent = document.querySelector('.persent');
+var optState = false;
+var opt='';
+var final= 0;
+
